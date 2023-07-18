be87450271f55aeb9aa72c29d94d13646fa0df07
Maxim Kolpashchikov <m.kolpashchikov@okenit.ru>
2023-07-18 12:26:15 +0300

    Controllers fix

--- Server/src/main/kotlin/org/kolpaschikov/bank/server/config/CorsConfig.kt
+++ Server/src/main/kotlin/org/kolpaschikov/bank/server/config/CorsConfig.kt
@@ -1,14 +1,12 @@
-package org.kolpaschikov.bank.server.config;
+package org.kolpaschikov.bank.server.config
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*");
+class CorsConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**").allowedMethods("*")
     }
 }
